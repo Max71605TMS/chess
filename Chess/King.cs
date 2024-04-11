cbbd4baf49e65f5a4d8ee1e906bb0ee66dc98be9
Kpoxu4 <159006608+Kpoxu4@users.noreply.github.com>
2024-04-11 15:35:46 +0300

    укоплектовал Сделал нормальный метод для перемещения

--- Chess/King.cs
+++ Chess/King.cs
@@ -1,110 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Chess
 {
     public class King : Figure
     {
-        private const int BoardSize = 8; // размер шахматной доски
+        private const int BoardSize = 8;
 
-        public bool _isInInitialPositon = true; //начальная позиция
-
+        public bool _isInInitialPositon = true;
 
         public King(bool isWhite, Point point) : base(isWhite, point)
         {
-
         }
 
-        public override IEnumerable<Point> GetAvaliablePositions(IEnumerable<Figure> figures) //Получить доступную позицию
+        public override IEnumerable<Point> GetAvaliablePositions(IEnumerable<Figure> figures)
         {
-            var allTheKingMoves = AllTheKingMoves(figures); // все возможные ходы короля без учета фигур
-            
-            return allTheKingMoves;
+            var allTheKingMoves = AllTheKingMoves();
+            return allTheKingMoves.Where(p => p.X >= 0 && p.X < BoardSize && p.Y >= 0 && p.Y < BoardSize);
         }
 
-        public override Image GetImage() // получение картинки фигуры
+        public override Image GetImage()
         {
-            if (isWhite)
-            {
-                if (isChoosen)
-                {
-                    return Properties.Resources.King_White_Green;
-                } else
-                {
-                    return (Position.X + Position.Y) % 2 == 0 ? Properties.Resources.King_White_White : Properties.Resources.King_White_Black;
-                }
-                
-            } else
-            {
-                if (isChoosen)
-                {
-                    return Properties.Resources.King_Black_Green;
-                }
-                else
-                {
-                    return (Position.X + Position.Y) % 2 == 0 ? Properties.Resources.King_Black_White : Properties.Resources.King_Black_Black;
-                }
-            }
+            var imageName = isWhite
+                ? (isChoosen ? "King_White_Green" : (Position.X + Position.Y) % 2 == 0 ? "King_White_White" : "King_White_Black")
+                : (isChoosen ? "King_Black_Green" : (Position.X + Position.Y) % 2 == 0 ? "King_Black_White" : "King_Black_Black");
+
+            return (Image)Properties.Resources.ResourceManager.GetObject(imageName);
         }
 
-        private List<Point> AllTheKingMoves(IEnumerable<Figure> figures)
+        private List<Point> AllTheKingMoves()
         {
-            var allTheKingMoves = new List<Point>();
+            var allTheKingMoves = new List<Point>
+            {
+                new Point(Position.X, Position.Y + 1),
+                new Point(Position.X, Position.Y - 1),
+                new Point(Position.X + 1, Position.Y),
+                new Point(Position.X - 1, Position.Y),
+                new Point(Position.X + 1, Position.Y - 1),
+                new Point(Position.X + 1, Position.Y + 1),
+                new Point(Position.X - 1, Position.Y + 1),
+                new Point(Position.X - 1, Position.Y - 1)
+            };
 
-            var point = new Point(Position.X, Position.Y + 1);
-            if (Position.Y < BoardSize - 1 )
-            {
-                allTheKingMoves.Add(new Point(Position.X, Position.Y + 1));
-            }
-            point = new Point(Position.X, Position.Y - 1);
-            if (Position.Y < BoardSize - 1)
-            {
-                allTheKingMoves.Add(new Point(Position.X, Position.Y - 1));
-            }
-            point = new Point(Position.X + 1, Position.Y);
-            if (Position.X < BoardSize - 1)
-            {
-                allTheKingMoves.Add(new Point(Position.X + 1, Position.Y));
-            }
-            point = new Point(Position.X - 1, Position.Y);
-            if (Position.X < BoardSize - 1)
-            {
-                allTheKingMoves.Add(new Point(Position.X - 1, Position.Y));
-            }
-            point = new Point(Position.X + 1, Position.Y - 1);
-            if (Position.X < BoardSize - 1 && Position.Y < BoardSize - 1)
-            {
-                allTheKingMoves.Add(new Point(Position.X + 1, Position.Y - 1));
-            }
-            point = new Point(Position.X + 1, Position.Y + 1);
-            if (Position.X < BoardSize - 1 && Position.Y < BoardSize - 1)
-            {
-                allTheKingMoves.Add(new Point(Position.X + 1, Position.Y + 1));
-            }
-            point = new Point(Position.X - 1, Position.Y + 1);
-            if (Position.X < BoardSize - 1 && Position.Y < BoardSize - 1)
-            {
-                allTheKingMoves.Add(new Point(Position.X - 1, Position.Y + 1));
-            }
-            point = new Point(Position.X - 1, Position.Y - 1);
-            if (Position.X < BoardSize - 1 && Position.Y < BoardSize - 1)
-            {
-                allTheKingMoves.Add(new Point(Position.X - 1, Position.Y - 1));
-            }
-
-            //ракеровка! добавить поля в Castle с _isInInitialPositon в проверку
-
-            if (_isInInitialPositon /*&& Castle._isInInitialPositon*/)
+            if (_isInInitialPositon)
             {
                 allTheKingMoves.Add(new Point(Position.X + 2, Position.Y));
-            }
-
-            if (_isInInitialPositon /*&& Castle._isInInitialPositon*/)
-            {
                 allTheKingMoves.Add(new Point(Position.X - 2, Position.Y));
             }
 
