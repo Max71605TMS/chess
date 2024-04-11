541fd81e036a2be737ffc8cb4f3ee90cf48f57c3
Илья <@DESKTOP-8ICT6FH>
2024-04-11 17:28:09 +0300

    Add GetImage Bishop

---
+++ Chess/Bishop.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    internal class Bishop : Figure
+    {
+        public Bishop(bool isWhite, Point point) : base(isWhite, point)
+        {
+        }
+
+        public override IEnumerable<Point> GetAvaliablePositions(IEnumerable<Figure> figures)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Image GetImage()
+        {
+            if (isWhite)
+            {
+                if(isChoosen)
+                {
+                    return Properties.Resources.Bishop_Black_Green;
+                }
+                else
+                {
+                    return (Position.X + Position.Y) % 2 == 0 ? Properties.Resources.Bishop_White_White : Properties.Resources.Bishop_White_Black;
+                }
+            }
+            else
+            {
+                if (isChoosen)
+                {
+                    return Properties.Resources.Bishop_Black_Green;
+                }
+                else
+                {
+                    return (Position.X + Position.Y) % 2 == 0 ? Properties.Resources.Bishop_Black_White : Properties.Resources.Castle_Black_Black;
+                }
+                
+
+            }
+        }
+    }
+}
