6d037f7c33f96b5583e5e0e6397be97724480740
Ancient <atlantis159@mail.ru>
2024-04-13 15:57:38 +0500

    Add IMoveByDirection and change Rook movement

---
+++ Chess/Interfaces/IMoveByDirection.cs
@@ -0,0 +1,30 @@
+﻿namespace Chess.Interfaces;
+
+public interface IMoveByDirection
+{
+    public IEnumerable<Point> GetPositionsByDirection(IEnumerable<Figure> figures, Figure figure, int xDirection, int yDirection)
+    {
+        var positions = new List<Point>();
+        var x = figure.Position.X + xDirection;
+        var y = figure.Position.Y + yDirection;
+
+        while (x is >= 0 and <= 7 && y is >= 0 and <= 7)
+        {
+            var figureOnTheWay = figures.FirstOrDefault(f => f.Position == new Point(x, y));
+
+            if (figureOnTheWay is not null)
+            {
+                if (figureOnTheWay.isWhite != figure.isWhite)
+                    positions.Add(new Point(x, y));
+                break;
+            }
+
+            positions.Add(new Point(x, y));
+
+            x += xDirection;
+            y += yDirection;
+        }
+
+        return positions;
+    }
+}
--- Chess/Rook.cs
+++ Chess/Rook.cs
@@ -3,7 +3,7 @@ using Chess.Properties;
 
 namespace Chess;
 
-public class Rook : Figure, IMarkable
+public class Rook : Figure, IMarkable, IMoveByDirection
 {
     public bool IsFirstTurn { get; set; } = true;
 
@@ -15,36 +15,10 @@ public class Rook : Figure, IMarkable
     {
         var positions = new List<Point>();
 
-        positions.AddRange(GetPositionsByDirection(figures, -1, 0)); // По горизонтали слева
-        positions.AddRange(GetPositionsByDirection(figures, 1, 0));  // По горизонтали справа
-        positions.AddRange(GetPositionsByDirection(figures, 0, -1)); // По вертикали сверху
-        positions.AddRange(GetPositionsByDirection(figures, 0, 1));  // По вертикали снизу
-
-        return positions;
-    }
-
-    private IEnumerable<Point> GetPositionsByDirection(IEnumerable<Figure> figures, int xDirection, int yDirection)
-    {
-        var positions = new List<Point>();
-        var x = Position.X + xDirection;
-        var y = Position.Y + yDirection;
-
-        while (x is >= 0 and <= 7 && y is >= 0 and <= 7)
-        {
-            var figure = figures.FirstOrDefault(f => f.Position == new Point(x, y));
-
-            if (figure is not null)
-            {
-                if (figure.isWhite != isWhite)
-                    positions.Add(new Point(x, y));
-                break;
-            }
-
-            positions.Add(new Point(x, y));
-
-            x += xDirection;
-            y += yDirection;
-        }
+        positions.AddRange(((IMoveByDirection)this).GetPositionsByDirection(figures, this, -1, 0)); // По горизонтали слева
+        positions.AddRange(((IMoveByDirection)this).GetPositionsByDirection(figures, this, 1, 0));  // По горизонтали справа
+        positions.AddRange(((IMoveByDirection)this).GetPositionsByDirection(figures, this, 0, -1)); // По вертикали сверху
+        positions.AddRange(((IMoveByDirection)this).GetPositionsByDirection(figures, this, 0, 1));  // По вертикали снизу
 
         return positions;
     }
