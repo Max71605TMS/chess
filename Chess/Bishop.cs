823afa98f081a2effcb8668c40017a98320a3de3
Илья <@DESKTOP-8ICT6FH>
2024-04-13 18:46:16 +0300

    Add GetAvaliablePositions Bishop

--- Chess/Bishop.cs
+++ Chess/Bishop.cs
@@ -8,15 +8,26 @@ using System.Threading.Tasks;
 
 namespace Chess
 {
-    internal class Bishop : Figure
+    public class Bishop : Figure, IMarkable
     {
+        private const int BoardSize = 8;
+
+        public bool IsFirstTurn { get; set; } = true;
+
         public Bishop(bool isWhite, Point point) : base(isWhite, point)
         {
         }
 
         public override IEnumerable<Point> GetAvaliablePositions(IEnumerable<Figure> figures)
         {
-            throw new NotImplementedException();
+            var positions = new List<Point>();
+
+            positions.AddRange(GetPositionsByDirection(figures, -1, -1)); // По горизонтали слева
+            positions.AddRange(GetPositionsByDirection(figures, 1, 1));  // По горизонтали справа
+            positions.AddRange(GetPositionsByDirection(figures, 1, -1)); // По вертикали сверху
+            positions.AddRange(GetPositionsByDirection(figures, -1, 1));  // По вертикали снизу
+
+            return positions;
         }
 
         public override Image GetImage()
@@ -44,6 +55,32 @@ namespace Chess
                     return (Position.X + Position.Y) % 2 == 0 ? Properties.Resources.Bishop_Black_White : Properties.Resources.Bishop_Black_Black;
                 }
             }
+        }
+
+        private IEnumerable<Point> GetPositionsByDirection(IEnumerable<Figure> figures, int xDirection, int yDirection)
+        {
+            var positions = new List<Point>();
+            var x = Position.X + xDirection;
+            var y = Position.Y + yDirection;
+
+            while (x is >= 0 and <= 7 && y is >= 0 and <= 7)
+            {
+                var figure = figures.FirstOrDefault(f => f.Position == new Point(x, y));
+
+                if (figure is not null)
+                {
+                    if (figure.isWhite != isWhite)
+                        positions.Add(new Point(x, y));
+                    break;
+                }
+
+                positions.Add(new Point(x, y));
+
+                x += xDirection;
+                y += yDirection;
+            }
+
+            return positions;
         }
     }
 }
