b41c0882b99efdc2c687e67e7bd77c4fe70ee3b2
Илья <@DESKTOP-8ICT6FH>
2024-04-14 18:26:41 +0300

    Add interfaces Bishop

--- Chess/Bishop.cs
+++ Chess/Bishop.cs
@@ -8,24 +8,21 @@ using System.Threading.Tasks;
 
 namespace Chess
 {
-    public class Bishop : Figure, IMarkable
+    public class Bishop : Figure, IMovementBishop
     {
-        private const int BoardSize = 8;
-
-        public bool IsFirstTurn { get; set; } = true;
-
         public Bishop(bool isWhite, Point point) : base(isWhite, point)
         {
+
         }
 
         public override IEnumerable<Point> GetAvaliablePositions(IEnumerable<Figure> figures)
         {
             var positions = new List<Point>();
 
-            positions.AddRange(GetPositionsByDirection(figures, -1, -1)); // По горизонтали слева
-            positions.AddRange(GetPositionsByDirection(figures, 1, 1));  // По горизонтали справа
-            positions.AddRange(GetPositionsByDirection(figures, 1, -1)); // По вертикали сверху
-            positions.AddRange(GetPositionsByDirection(figures, -1, 1));  // По вертикали снизу
+            positions.AddRange(((IMovementBishop)this).GetMove(figures, this, -1, 1)); 
+            positions.AddRange(((IMovementBishop)this).GetMove(figures, this, 1, 1));  
+            positions.AddRange(((IMovementBishop)this).GetMove(figures, this, 1, -1)); 
+            positions.AddRange(((IMovementBishop)this).GetMove(figures, this, -1, -1));  
 
             return positions;
         }
@@ -42,7 +39,6 @@ namespace Chess
                 {
                     return (Position.X + Position.Y) % 2 == 0 ? Properties.Resources.Bishop_White_White : Properties.Resources.Bishop_White_Black;
                 }
-
             }
             else
             {
@@ -55,32 +51,6 @@ namespace Chess
                     return (Position.X + Position.Y) % 2 == 0 ? Properties.Resources.Bishop_Black_White : Properties.Resources.Bishop_Black_Black;
                 }
             }
-        }
-
-        private IEnumerable<Point> GetPositionsByDirection(IEnumerable<Figure> figures, int xDirection, int yDirection)
-        {
-            var positions = new List<Point>();
-            var x = Position.X + xDirection;
-            var y = Position.Y + yDirection;
-
-            while (x is >= 0 and <= 7 && y is >= 0 and <= 7)
-            {
-                var figure = figures.FirstOrDefault(f => f.Position == new Point(x, y));
-
-                if (figure is not null)
-                {
-                    if (figure.isWhite != isWhite)
-                        positions.Add(new Point(x, y));
-                    break;
-                }
-
-                positions.Add(new Point(x, y));
-
-                x += xDirection;
-                y += yDirection;
-            }
-
-            return positions;
         }
     }
 }
