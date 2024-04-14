d491379093e30d742e5b9da0f16ab6c1495eecaa
Sergei <sergeigulyako@mail.ru>
2024-04-15 02:11:22 +0300

    added pawn attack

--- Chess/Pawn.cs
+++ Chess/Pawn.cs
@@ -19,49 +19,56 @@ namespace Chess
 
         public bool IsFirstTurn { get; set; } = true;
 
-        private bool IsPositionOccupied(IEnumerable<Figure> figures, Point position)
-        {
-            return figures.Any(f => f.Position == position);
-        }
-
-        private void AddPositionIfFree(List<Point> positions, IEnumerable<Figure> figures, Point position)
-        {
-            if (!IsPositionOccupied(figures, position))
-            {
-                positions.Add(position);
-            }
-        }
-
         public override IEnumerable<Point> GetAvaliablePositions(IEnumerable<Figure> figures)
-        {
+        {       
             var positions = new List<Point>();
-
-            var forwardStep = _isDirectionUp ? -1 : 1;
-            var doubleForwardStep = _isDirectionUp ? -2 : 2;
-
-            var forwardPosition = new Point(Position.X, Position.Y + forwardStep);
-            var doubleForwardPosition = new Point(Position.X, Position.Y + doubleForwardStep);
-
-            var isAnyFiguresBehind = IsPositionOccupied(figures, forwardPosition);
-            var attackingPositions = figures.Where(f =>
-                f.Position == new Point(Position.X - 1, Position.Y + forwardStep) ||
-                f.Position == new Point(Position.X + 1, Position.Y + forwardStep)).ToList();
-
-            if (Position.Y != 1 && Position.Y != BoardSize)
+            if (_isDirectionUp && isWhite)
             {
-                AddPositionIfFree(positions, figures, forwardPosition);
-
-                foreach (var attackingPosition in attackingPositions)
+                var isAnyFiguresBehind = figures.Any(f => f.Position == new Point(Position.X, Position.Y - 1));
+                var attackingPositions = figures.Where(f => f.Position == new Point(Position.X - 1, Position.Y - 1) 
+                || f.Position == new Point(Position.X + 1, Position.Y - 1)).ToList();
+                
+                if (Position.Y > 1)
                 {
-                    positions.Add(attackingPosition.Position);
+                    if(!isAnyFiguresBehind)
+                    {
+                        positions.Add(new Point(Position.X, Position.Y - 1));                        
+                    }                    
+                    foreach (var attackingPosition in attackingPositions)
+                    {
+                        positions.Add(attackingPosition.Position);
+                    }
                 }
-
-                if (IsFirstTurn && !isAnyFiguresBehind && !IsPositionOccupied(figures, doubleForwardPosition))
+                var isAnyFiguresBehindPlus = figures.Any(f => f.Position == new Point(Position.X, Position.Y - 2));
+                if (IsFirstTurn && !isAnyFiguresBehind && !isAnyFiguresBehindPlus)
                 {
-                    positions.Add(doubleForwardPosition);
+                    positions.Add(new Point(Position.X, Position.Y - 2));
                 }
-            }
+                
+            } else
+            {
+                var isAnyFiguresBehind = figures.Any(f => f.Position == new Point(Position.X, Position.Y + 1));
+                var attackingPositions = figures.Where(f => f.Position == new Point(Position.X + 1, Position.Y + 1)
+                || f.Position == new Point(Position.X - 1, Position.Y + 1)).ToList();
+                if (Position.Y < BoardSize)
+                {
+                    if(!isAnyFiguresBehind)
+                    {
+                        positions.Add(new Point(Position.X, Position.Y + 1));                        
+                    }
+                    foreach (var attackingPosition in attackingPositions)
+                    {
+                        positions.Add(attackingPosition.Position);
+                    }
 
+                }
+                var isAnyFiguresBehindPlus = figures.Any(f => f.Position == new Point(Position.X, Position.Y + 2));
+                if (IsFirstTurn && !isAnyFiguresBehind && !isAnyFiguresBehindPlus)
+                {
+                    positions.Add(new Point(Position.X, Position.Y + 2));
+                }
+            }            
+            //add all positions
             return positions;
         }
 
