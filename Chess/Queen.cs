bc7926bcbee38c0224a534b4227c80678a41a612
TatianaNai <tatyana_tat333@mail.ru>
2024-04-14 21:26:41 +0300

    Image Queen changed

--- Chess/Queen.cs
+++ Chess/Queen.cs
@@ -38,12 +38,12 @@ namespace Chess
                 if (IsChoosen)
                     return Resources.Queen_White_Green;
 
-                return (Position.X + Position.Y) % 2 == 0 ? Resources.Queen_White_White : Resources.Queen_White_Black;
+                return (Position.X + Position.Y) % 2 == 0 ? Resources.Queen_White_White : Resources.Queen_White_Black_2;
             }
 
             if (IsChoosen)
-                return Resources.Queen_Black_Green;
-            return (Position.X + Position.Y) % 2 == 0 ? Resources.Queen_Black_White : Resources.Queen_Black_Black;
+                return Resources.Queen_Black_Green_2;
+            return (Position.X + Position.Y) % 2 == 0 ? Resources.Queen_Black_White_2 : Resources.Queen_Black_Black;
         }
     }
 }
