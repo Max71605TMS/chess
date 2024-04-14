d3336a1160029a2fa3c06abc04a4b39ba4be0492
Kpoxu4 <159006608+Kpoxu4@users.noreply.github.com>
2024-04-14 16:37:32 +0300

    check method WalkStraight

--- Chess/Figure.cs
+++ Chess/Figure.cs
@@ -27,5 +27,43 @@ namespace Chess
 
         public abstract Image GetImage();
 
+        public  IEnumerable<Point> WalkStraight(IEnumerable<Figure> figures)
+        {
+            var positions = new List<Point>();
+
+            positions.AddRange(GetPositionsByDirection(figures, -1, 0)); // По горизонтали слева
+            positions.AddRange(GetPositionsByDirection(figures, 1, 0));  // По горизонтали справа
+            positions.AddRange(GetPositionsByDirection(figures, 0, -1)); // По вертикали сверху
+            positions.AddRange(GetPositionsByDirection(figures, 0, 1));  // По вертикали снизу
+
+            return positions;
+        }
+
+        private  IEnumerable<Point> GetPositionsByDirection(IEnumerable<Figure> figures, int xDirection, int yDirection)
+        {
+            var positions = new List<Point>();
+            var x =Position.X + xDirection;
+            var y = Position.Y + yDirection;
+
+            while (x is >= 0 and <= 7 && y is >= 0 and <= 7)
+            {
+                var figure = figures.FirstOrDefault(f => f.Position == new Point(x, y));
+
+                if (figure is not null)
+                {
+                    if (figure.IsWhite != IsWhite)
+                        positions.Add(new Point(x, y));
+                    break;
+                }
+
+                positions.Add(new Point(x, y));
+
+                x += xDirection;
+                y += yDirection;
+            }
+
+            return positions;
+        }
+
     }
 }
